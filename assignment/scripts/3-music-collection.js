d6e46700bea5e02d3e976c6cf402321371e7e967
Everett Butler <everettjamesbutler@gmail.com>
2023-02-07 16:30:37 -0600

    finished up my findByArtist function and testing! May do another commit just to clean up some code and make some more comments! Cheers!

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -13,18 +13,36 @@ function addToCollection(title, artist, yearPublished) {
     return collection;
 }
 
-addToCollection('Shadows Collide with People', 'John Frusciante',2004);
+addToCollection('Shadows Collide with People', 'John Frusciante', 2004);
 addToCollection('Niandra LaDes and Usually Just a T-Shirt', 'John Frusciante', 1994);
 addToCollection('Is This It', 'The Strokes', 2001);
 addToCollection('Grace', 'Jeff Buckley', 1994);
 addToCollection('WHEN WE ALL FALL ASLEEP, WHERE DO WE GO?', 'Billie Eilish', 2019);
 addToCollection('EL ÚLTIMO TOUR DEL MUNDO', 'Bad Bunny', 2020);
 
-// console.log(collection);
-// console.table(collection);
+console.log(collection);
+
+console.table(collection) //dont forget about tables! Really good way to view information!
+
+console.log(collection.length)
+
 function showCollection() {
-    console.table(collection)
+    for (let i = 0; i < collection.length; i++) {
+        console.log(`${collection[i].title} by ${collection[i].artist} published in ${collection[i].yearPublished}`);
+    }
 }
 
 showCollection();
-// console.log(collection);
+
+function findByArtist(artist) {
+    let matchingArtist = [];
+    for (let i = 0; i < collection.length; i++)
+        if (artist === collection[i].artist) {
+            matchingArtist.push(artist);
+        }
+    return console.log(matchingArtist)
+}
+
+findByArtist('Bad Bunny');
+findByArtist('John Frusciante');
+findByArtist('Red Hot Chili Peppers'); //this was just to check all the final parameters
