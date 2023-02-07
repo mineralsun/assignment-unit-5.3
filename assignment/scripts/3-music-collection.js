48c31997654f17132e6d81bed67b92c71e82cc9d
Everett Butler <everettjamesbutler@gmail.com>
2023-02-06 21:10:00 -0600

    drafting my initial function and wanting to save before I forget!

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,17 @@
 console.log('***** Music Collection *****')
+let collectionGlobal = '' // put global variable up here possibly?
+
+function addToCollection(title, artist, yearPublished) {
+    let collection = [
+        {
+        title: '',
+        artist: '',
+        yearPublished: '',
+
+        }
+    ]
+    
+}
+
+addToCollection('Shadows Collide with People', 'John Frusciante', '2004');
+console.log(collectionGlobal);
