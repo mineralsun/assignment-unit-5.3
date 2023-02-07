d4ffdac2ad6b278876ca6ad0baff9e0a8398f47a
Everett Butler <everettjamesbutler@gmail.com>
2023-02-07 15:21:23 -0600

    fixed my 'add to collection' function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,17 +1,30 @@
 console.log('***** Music Collection *****')
 let collectionGlobal = '' // put global variable up here possibly?
+let collection = [];
+
 
 function addToCollection(title, artist, yearPublished) {
-    let collection = [
-        {
-        title: '',
-        artist: '',
-        yearPublished: '',
-
-        }
-    ]
-    
+    let albumData = {
+        title: title,
+        artist, //shorthand notation of the other examples
+        yearPublished: yearPublished,
+    }
+    collection.push(albumData);
+    return collection;
 }
 
-addToCollection('Shadows Collide with People', 'John Frusciante', '2004');
-console.log(collectionGlobal);
+addToCollection('Shadows Collide with People', 'John Frusciante',2004);
+addToCollection('Niandra LaDes and Usually Just a T-Shirt', 'John Frusciante', 1994);
+addToCollection('Is This It', 'The Strokes', 2001);
+addToCollection('Grace', 'Jeff Buckley', 1994);
+addToCollection('WHEN WE ALL FALL ASLEEP, WHERE DO WE GO?', 'Billie Eilish', 2019);
+addToCollection('EL ÚLTIMO TOUR DEL MUNDO', 'Bad Bunny', 2020);
+
+// console.log(collection);
+// console.table(collection);
+function showCollection() {
+    console.table(collection)
+}
+
+showCollection();
+// console.log(collection);
